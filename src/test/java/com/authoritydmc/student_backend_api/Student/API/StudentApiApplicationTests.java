cc89de714790cff04b5adc070df25670f7051ac6
Raj Dubey <com.mailuser@gmail.com>
2022-06-30 20:06:37 +0530

    Initial commit

---
+++ src/test/java/com/authoritydmc/student_backend_api/Student/API/StudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authoritydmc.student_backend_api.Student.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
